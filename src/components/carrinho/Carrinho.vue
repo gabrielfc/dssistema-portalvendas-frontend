aa386221b2e1de43f5a8e163bce0dbd1172cc6b9
Gabriel Campolina <gabrielfc@gmail.com>
2020-07-18 16:21:09 -0300

    Correção layout pagamento

--- src/components/carrinho/Carrinho.vue
+++ src/components/carrinho/Carrinho.vue
@@ -4,62 +4,55 @@
       <h2 class="text-center">
         <strong>Carrinho</strong>
       </h2>
-      <div class="col-md-12 col-lg-8">
-        <div class="items">
-          <div class="product">
-            <div class="row justify-content-center align-items-center" style="padding: 10px;">
-              <div class="col-md-2">
-                <label class="d-none d-md-block">Plano</label>
-                Ouro especial
-              </div>
-              <div class="col-md-2 price">
-                <label class="d-none d-md-block">Valor por pessoa</label>
-                <span>R$120,00</span>
-              </div>
-              <div class="col-md-2">
-                <label class="d-none d-md-block">Pessoas</label>
-                <input
-                  type="number"
-                  id="number"
-                  class="form-control quantity-input"
-                  value="1"
-                  :disabled="!this.$props.showActions"
-                />
-              </div>
-              <div class="col-md-2">
-                <label class="d-none d-md-block">Cobrança</label>
-                <select class="form-control" :disabled="!this.$props.showActions">
-                  <optgroup label="Mensal">
-                    <option value="12" selected>Mensal</option>
-                    <option value="13">Semetral</option>
-                    <option value="14">Anual</option>
-                  </optgroup>
-                </select>
-              </div>
-              <div class="col-md-2">
-                <button class="btn btn-danger" type="button" v-if="this.$props.showActions">Excluir</button>
-              </div>
-            </div>
-          </div>
+
+      <div class="carrinho">
+        <div class="carrinho-box">
+          <strong>Plano</strong>
+          <span>Plano ouro expecial</span>
         </div>
-      </div>
-      <div class="col-md-12 col-lg-4">
-        <div class="summary" style="margin-right: 29px;">
-          <h3 style="padding: 16px;margin-right: 5px;">Resumo</h3>
-          <h4></h4>
-          <h4></h4>
-          <h4></h4>
-          <h4>
-            <span class="text">Total</span>
-            <span class="price">R$ 120,00</span>
-          </h4>
+        <div class="carrinho-box">
+          <strong>Valor por pessoa</strong>
+          <span>R$ 120,00</span>
+        </div>
+        <div class="carrinho-box">
+          <strong>Qtd. Pessoa</strong>
+          <input
+            type="number"
+            id="number"
+            class="form-control quantity-input"
+            value="3"
+            :disabled="!this.$props.showActions"
+          />
+        </div>
+        <div class="carrinho-box">
+          <strong>Cobrança</strong>
+          <select class="form-control" :disabled="!this.$props.showActions">
+            <optgroup label="Mensal">
+              <option value="12" selected>Mensal</option>
+              <option value="13">Semetral</option>
+              <option value="14">Anual</option>
+            </optgroup>
+          </select>
+        </div>
+        <div class="carrinho-box" style="margin-top: 23px">
+          <b-button>X</b-button>
         </div>
 
-        <b-button
-          class="button-planos"
-          @click="finalizarCompra()"
-          v-if="this.$props.showActions"
-        >Finalizar Compra</b-button>
+        <div>
+          <div class="carrinho-box">
+            <h3 style="padding: 16px;margin-right: 5px;">Total</h3>
+            <h4></h4>
+            <h4>
+              <span class="price">R$ 360,00</span>
+            </h4>
+          </div>
+
+          <b-button
+            class="button-planos"
+            @click="finalizarCompra()"
+            v-if="this.$props.showActions"
+          >Finalizar Compra</b-button>
+        </div>
       </div>
     </form>
   </div>
@@ -87,4 +80,17 @@ export default {
 </script>
 
 <style >
+.carrinho {
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 60px;
+}
+.carrinho-box {
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  align-items: center;
+}
 </style>
--- src/components/pagamento/Pagamento.vue
+++ src/components/pagamento/Pagamento.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="form-container" style="margin-top:20px">
+  <div class="form-container" style="margin-top:20px;">
     <form method="post">
       <h2 class="text-center">
         <strong>Pagamento</strong>
       </h2>
 
-      <img src="@/assets/cartao.png" width="90px" height="90px" />
+      <b-form-radio-group v-model="formaPagamento">
+        <b-form-radio value="1">
+          <img src="@/assets/cartao.png" width="120px" height="90px" />
+        </b-form-radio>
 
-      <img src="@/assets/boleto.png" width="90px" height="90px" />
+        <b-form-radio value="2">
+          <img src="@/assets/boleto.png" width="120px" height="90px" />
+        </b-form-radio>
+      </b-form-radio-group>
 
-      <b-button class="button-planos" @click="finalizarCompra()">Finalizar Compra</b-button>
+      <div v-if="formaPagamento == '1'">
+        <div class="form-group">
+          <input class="form-control" type="text" placeholder="Cartão" style="margin-top: 25px;" />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" placeholder="Nome do titular" />
+        </div>
+        <div class="form-group form-inline">
+          <input class="form-control" type="text" placeholder="Vencimento" />
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Código de segurança"
+            style="margin-left: 15px;"
+          />
+        </div>
+      </div>
+
+      <b-button
+        class="button-planos"
+        @click="finalizarCompra()"
+        style="margin-top: 25px;"
+      >Finalizar Compra</b-button>
     </form>
   </div>
 </template>
@@ -21,7 +49,9 @@ import axios from "axios";
 export default {
   name: "Pagamento",
   data: function() {
-    return {};
+    return {
+      formaPagamento: ""
+    };
   },
   methods: {
     finalizarCompra() {}
